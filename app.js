2501cd16c701814ea3311cb6d9798d05a37dbb9c
ISAAC RITHARSON <81296265+staroIR11@users.noreply.github.com>
2023-05-21 15:01:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('#input-form');
+const query = document.querySelector('#query');
+const wordcount = document.querySelector('#wordcount');
+const urls = document.querySelector('#urls');
+const summary = document.querySelector('#summary');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	fetch('/summarize', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			query: query.value,
+			wordcount: wordcount.value,
+			urls: urls.value
+		})
+	})
+	.then(response => response.json())
+	.then(data => {
+		summary.innerHTML = data.summary;
+	})
+	.catch(error => console.error(error));
+});
